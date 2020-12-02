b4fa915421a6b1b9ab3d558c1810ed12162a9e30
Gleb Bayeshko <bayeshko_gleb@mail.ru>
2020-12-02 14:14:10 +0300

    fix: change default route in server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.get("/api/words", async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../dist/index.html`));
 });
 
